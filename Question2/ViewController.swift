342010ea613776c7b1c5918bfca5068532db33d8
Iulian Costin Bujniga <iulian_costin19@yahoo.com>
2020-02-29 18:04:49 -0500

    Initial Commit

---
+++ Question2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Question2
+//
+//  Created by iulian on 2/29/20.
+//  Copyright © 2020 ___IULIAN COSTIN BUJNIGA ___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
